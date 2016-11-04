3d931a054210db7e5c8cef6363c9ff8b989985aa
KushalChandak <kpchandak500@gmail.com>
2016-11-04 22:04:21 +0530

    Update sizecmp.c

--- sizecmp.c
+++ sizecmp.c
@@ -1,6 +1,6 @@
 /*************************************************************************
 *   <fdupes_command>
-*   Copyright (C) 2016  Kushal Purushottam Chandak kpchandak500@gmail.com
+*   Copyright (C) 2017  Kushal Purushottam Chandak kpchandak500@gmail.com
 *
 *   This program is free software: you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
