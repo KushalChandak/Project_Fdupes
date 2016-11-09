889ee2e94904cadb45a28f6a3ddceb2fb3dbb372
KushalChandak <kpchandak500@gmail.com>
2016-11-09 13:52:48 +0530

    Update bytecmp.c

--- bytecmp.c
+++ bytecmp.c
@@ -24,9 +24,9 @@ int bytecmp(char *argv1, char*argv2) {
 	fp1=fopen(argv2,"r");
 	int x, y;
 	while((x=fgetc(fp))!=EOF && (y=fgetc(fp1))!=EOF){
-			//	printf("%c \t %c \n",x,y);
+				
 	if(x!=y){
-			//printf("Not A Duplicate\n");
+			
 			return 1;
 		}
 		
